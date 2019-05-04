b6e04b696b03d6758230444728fab50e3ee65078
段佳利 <jli.duan@gmail.com>
2019-05-03 19:02:54 -0700

    init

---
+++ cmake-build-debug/CMakeFiles/design_principle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/design_principle.dir/main.cpp.o"
+  "design_principle.pdb"
+  "design_principle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/design_principle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
